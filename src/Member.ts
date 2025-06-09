976ed13d0c8b75d6024081ea9fc6b8f3741a6189
quiquelhappy <quiquelhappy@gmail.com>
2025-06-09 13:31:13 +0200

    added vote banning parsing

--- src/Member.ts
+++ src/Member.ts
@@ -6,15 +6,17 @@ export default class Member {
     public readonly name: string
     public readonly eid: string
     public readonly id: string
+    public readonly voteBanned: Date | null = null
 
-    constructor(client: Serverbench, name: string, eid: string, id: string) {
+    constructor(client: Serverbench, name: string, eid: string, id: string, voteBanned: Date | null) {
         this.client = client
         this.name = name
         this.eid = eid
         this.id = id
+        this.voteBanned = voteBanned ? voteBanned : null
     }
 
     public static fromObject(client: Serverbench, object: any) {
-        return new Member(client, object.name, object.eid, object.id)
+        return new Member(client, object.name, object.eid, object.id, object.voteBanned ? new Date(object.voteBanned) : null)
     }
 }
